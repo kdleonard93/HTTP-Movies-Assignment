02a1fd526e93d37717255c95491240538ceda818
Kyle Leonard <kdleonard@icloud.com>
2020-01-21 18:43:36 -0600

    Got the form set up

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,16 +1,44 @@
-import React, {useState} from "react";
-import axios from "axios";
+import React, { useState } from "react";
 
 const initialState = {
-    id:"",
-    title:"",
-    director: "",
-    metascore: "",
-    stars: "",
-}
-
-const UpdateMovie = () => {
-    return();
+  id: null,
+  title: "",
+  director: "",
+  metascore: null,
+  stars: []
 };
+
+const UpdateMovie = () => {};
+
+return (
+  <>
+    <form>
+      <input
+        type="text"
+        name="title"
+        value={movie.title}
+        placeholder="Title"
+      ></input>
+      <input
+        type="text"
+        name="director"
+        value={movie.director}
+        placeholder="Director"
+      ></input>
+      <input
+        type="text"
+        name="stars"
+        value={movie.stars}
+        placeholder="Stars"
+      ></input>
+      <input
+        type="number"
+        name="metascore"
+        value={movie.metascore}
+        placeholder="Metascore"
+      ></input>
+    </form>
+  </>
+);
 
 export default UpdateMovie;

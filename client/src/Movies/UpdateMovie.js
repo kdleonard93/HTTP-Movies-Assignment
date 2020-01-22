13f14252baac84ed28fc6be35004fc7200f0eab6
Kyle Leonard <kdleonard@icloud.com>
2020-01-21 19:23:59 -0600

    Added onChange function

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -8,6 +8,16 @@ const initialState = {
   stars: []
 };
 
+const [movie, setMovie] = useState(initialState);
+
+const handleChange = e => {
+  setMovie({
+    ...movie,
+    [e.target.name]: [e.target.value]
+  });
+  console.log(movie);
+};
+
 const UpdateMovie = () => {};
 
 return (
@@ -18,24 +28,28 @@ return (
         name="title"
         value={movie.title}
         placeholder="Title"
+        onChange={handleChange}
       ></input>
       <input
         type="text"
         name="director"
         value={movie.director}
         placeholder="Director"
+        onChange={handleChange}
       ></input>
       <input
         type="text"
         name="stars"
         value={movie.stars}
         placeholder="Stars"
+        onChange={handleChange}
       ></input>
       <input
         type="number"
         name="metascore"
         value={movie.metascore}
         placeholder="Metascore"
+        onChange={handleChange}
       ></input>
     </form>
   </>
